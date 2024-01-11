23c6d848daa879ef813672308b48f8b409f686e9
Qi Shang <qishang1992@163.com>
2024-01-11 19:01:00 +0800

    看门狗

---
+++ 00_template_clion/Hardware/bsp_Wdgt.h
@@ -0,0 +1,12 @@
+//
+// Created by pc on 2024/1/11.
+//
+
+#ifndef INC_00_TEMPLATE_CLION_BSP_WDGT_H
+#define INC_00_TEMPLATE_CLION_BSP_WDGT_H
+
+#include "gd32f4xx.h"
+
+void fwtd_config();
+void wwtd_config();
+#endif //INC_00_TEMPLATE_CLION_BSP_WDGT_H
